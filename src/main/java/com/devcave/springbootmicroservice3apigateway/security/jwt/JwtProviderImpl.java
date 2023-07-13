ba1deb49f034d825c9fbb9a4628d2ddfeba34710
nog <gcoscar99@yahoo.com>
2023-07-13 12:48:09 -0600

    injection problem solved

--- src/main/java/com/devcave/springbootmicroservice3apigateway/security/jwt/JwtProviderImpl.java
+++ src/main/java/com/devcave/springbootmicroservice3apigateway/security/jwt/JwtProviderImpl.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
@@ -20,6 +21,7 @@ import java.util.Date;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Component
 public class JwtProviderImpl implements JwtProvider {
 
     @Value("${app.jwt.secret}")
